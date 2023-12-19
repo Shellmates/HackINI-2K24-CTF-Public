48237326fc20cbf76ee2e1bf1663f00c9cf71f56
Abderraouf-D <ka_dandani@esi.dz>
2023-12-19 13:50:55 +0100

    updated writeup

--- web/dumb_login/challenge/ocr.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
-
-def ocr(image_stream):
-    image = Image.open(image_stream)
-    text = pytesseract.image_to_string(image)
-    return text
